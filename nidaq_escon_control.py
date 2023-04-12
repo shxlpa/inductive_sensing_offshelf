63a22cfe5149bf276e05cb0bd6162bc0728e740c
raoshilpa <shilpa.padiki.rao@gmail.com>
2023-04-12 15:31:05 -0700

    writing to daq test

--- nidaq_escon_control.py
+++ nidaq_escon_control.py
@@ -4,4 +4,18 @@ with nidaqmx.Task() as task:
     task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
     task.read()
 
-# this is a test commit2
+# example of writing to daq
+# https://nidaqmx-python.readthedocs.io/en/latest/
+'''
+import nidaqmx
+from nidaqmx.types import CtrTime
+with nidaqmx.Task() as task:
+    task.co_channels.add_co_pulse_chan_time("Dev1/ctr0")
+    sample = CtrTime(high_time=0.001, low_time=0.001)
+    task.write(sample)
+
+1
+with nidaqmx.Task() as task:
+    task.ao_channels.add_ao_voltage_chan("Dev1/ao0")
+    task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
+'''
